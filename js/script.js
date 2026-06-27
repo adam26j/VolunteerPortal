1c3785e1591cfb8eb85ee8803c3901ecc4361954
Adam Aj <adamasabj@gmail.com>
2026-06-27 18:02:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const APPSCRIPT_URL = "https://script.google.com/macros/s/AKfycbznQqGQpJRRePQ5osbXoaB6lwql5O8FjEjO1Fkpwulmo5-3BBcbrL0-ztoqgTfliSXybQ/exec";
 
+document.getElementById("loginBtn").onclick = () => {
+
+window.open(APPSCRIPT_URL,"_blank");
+
+}
+
+document.getElementById("heroLogin").onclick = () => {
+
+window.open(APPSCRIPT_URL,"_blank");
+
+}
