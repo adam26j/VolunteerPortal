e6fce91dbd347d8621f97059d896424976f28dfa
Adam Aj <adamasabj@gmail.com>
2026-06-27 18:31:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,3 +47,18 @@ counter.style.transition="all .8s ease";
 observer.observe(counter);
 
 });
+window.addEventListener("scroll", () => {
+
+const header = document.querySelector(".header");
+
+if (window.scrollY > 50) {
+
+header.style.boxShadow = "0 15px 35px rgba(0,0,0,.12)";
+
+} else {
+
+header.style.boxShadow = "0 8px 25px rgba(0,0,0,.08)";
+
+}
+
+});
